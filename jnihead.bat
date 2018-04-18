9019076743151f1064c2513f4e5beb09668d0746
10km <10km0811@sohu.com>
2018-04-18 23:49:16 +0800

    update relative path

--- jnihead.bat
+++ jnihead.bat
@@ -1,4 +1,4 @@
 ECHO OFF
 pushd %~sdp0
-javah -v -classpath ..\faceapi-base\target\classes -d jni net.gdface.sdk.fse.FseJniBridge
+javah -v -classpath ..\faceapi\faceapi-base\target\classes -d jni net.gdface.sdk.fse.FseJniBridge
 popd
--- jnihead.sh
+++ jnihead.sh
@@ -1,3 +1,3 @@
 src_path=`cd "$(dirname $0)";pwd`
-javah -v -classpath $src_path/../faceapi-base/target/classes -d $src_path/jni net.gdface.sdk.fse.FseJniBridge
+javah -v -classpath $src_path/../faceapi/faceapi-base/target/classes -d $src_path/jni net.gdface.sdk.fse.FseJniBridge
 
