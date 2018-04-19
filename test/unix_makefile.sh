8fc11b7c0de56e246de16915e3ab82102ea81c6f
10km <10km0811@sohu.com>
2018-04-19 10:22:00 +0800

    commit

--- test/unix_makefile.sh
+++ test/unix_makefile.sh
@@ -10,18 +10,12 @@ then
 		exit -1
 	fi
 fi
-[ -n "$1" ] && INSTALL_FOLDER="$1"
-[ -z "$1" ] && INSTALL_FOLDER=$(dirname $(readlink -f $0))/release/fse_linux_x86_64
-# 默认INSTALL_FOLDER为源码文件夹下
-echo INSTALL_FOLDER:$INSTALL_FOLDER
 if [ -d build.gcc ]
 then 
 	rm -fr build.gcc/*
 else 
 	mkdir build.gcc
 fi
-pushd build.gcc
-cmake "`dirs +1`" $GXX_PATH -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_FOLDER
-make install
-popd
-rm -fr build.gcc
+cd build.gcc
+cmake $GXX_PATH -G "Unix Makefiles" -DFSE_ROOT_DIR=../../release/fse_linux_x86_64 ..
+
