4b378198a9b916cc6acecd0de85cf0b9c0c3f928
10km <10km0811@sohu.com>
2018-05-03 15:51:25 +0800

    删除fse_addFeature,fse_removeFeature函数，用fse_addFeatures，fse_removeFeatures统一 改进error_msg,使用std::string类型代替固定数组，避免字符长度溢出。

--- feature_se/feature_se.cpp
+++ feature_se/feature_se.cpp
@@ -11,15 +11,17 @@ using namespace gdface;
 // 搜索引擎对象，需要调用初始化函数初始化才有效
 static ICodeManager* instance = nullptr;
 // 当前线程的错误消息
-static thread_local char error_msg[256];
+static thread_local std::string error_msg;
 static string INVALID_ARGUMENT = "Invalid argument:";
 static string NULL_POINTER = "null pointer:";
 
+static void fill_error_msg(const std::string& msg) {
+	error_msg = msg;
+}
+
 static void fill_error_msg(const char* msg) {
 	if(nullptr != msg){
-		strncpy(error_msg, msg, std::min(sizeof(error_msg), strlen(msg)));
-		// 未尾截断
-		error_msg[std::min(strlen(msg) + 1,sizeof(error_msg) - 1)] = 0;
+		error_msg = msg;
 	}
 }
 // 将MD5数组转为MD5Set对象
@@ -43,7 +45,7 @@ static bool valid_instance() {
 	}
 }
 const char* fse_error_msg() {
-	return error_msg;
+	return error_msg.c_str();
 }
 
 int fse_init(uint32_t initCapacity, float loadFactor, size_t overBlockCapacity){
@@ -79,11 +81,11 @@ int fse_release() {
 
 int fse_searchCode(const face_code *code, double threshold, size_t rows, code_bean*out, const MD5 *md5Arrays,size_t md5Count) {
 	if (nullptr == code) {
-		fill_error_msg((NULL_POINTER + "code").c_str());
+		fill_error_msg(NULL_POINTER + "code");
 		return -1;
 	}
 	if (nullptr == out) {
-		fill_error_msg((NULL_POINTER + "out").c_str());
+		fill_error_msg(NULL_POINTER + "out");
 		return -1;
 	}
 	if (!valid_instance()) {
@@ -100,12 +102,11 @@ int fse_searchCode(const face_code *code, double threshold, size_t rows, code_be
 }
 
 int fse_getFeature(MD5 *md5, code_bean* out){
-	if (nullptr == md5) {
-		fill_error_msg((NULL_POINTER + "md5").c_str());
-		return -1;
+	if (is_null_MD5(md5)) {
+		return 0;
 	}
 	if (nullptr == out) {
-		fill_error_msg((NULL_POINTER + "out").c_str());
+		fill_error_msg(NULL_POINTER + "out");
 		return -1;
 	}
 	if (!valid_instance()) {
@@ -124,56 +125,29 @@ int fse_getFeature(MD5 *md5, code_bean* out){
 		return -1;
 	}
 }
-int fse_addFeature(code_bean* bean){
-	if (nullptr == bean) {
-		fill_error_msg((NULL_POINTER + "bean").c_str());
-		return -1;
-	}
-	if (!valid_instance()) {
-		return -1;
-	}
-	try{
-		instance->addBean(*bean);
-		return 0;
-	}	
-	catch (exception &e) {
-		fill_error_msg(e.what());
-		return -1;
-	}
-}
 int fse_addFeatures(code_bean *beans, size_t beanCount) {
 	if (nullptr == beans) {
-		fill_error_msg((NULL_POINTER + "beans").c_str());
-		return -1;
+		return 0;
 	}
 	if (!valid_instance()) {
 		return -1;
 	}
-	int addedCount = 0;
-	for (auto i = beanCount -1; i >= 0; --i, ++addedCount) {
-		if (fse_addFeature(beans + i) < 0) {
-			return -1;
-		}
-	}
-	return addedCount;
-}
-int fse_removeFeature(MD5 *md5) {
-	if (nullptr == md5) {
-		fill_error_msg((NULL_POINTER + "md5").c_str());
-		return -1;
-	}
-	if (!valid_instance()) {
-		return -1;
-	}
+	int count = 0;
 	try {
-		auto result = instance->removeBean(*md5);
-		return result ? 1 : 0;
+		for (decltype(beanCount) i = 0; i < beanCount; ++i) {
+			if (!beans[i].id.is_null()) {
+				instance->addBean(beans[i]);
+				++count;
+			}
+		}
 	}
 	catch (exception &e) {
 		fill_error_msg(e.what());
 		return -1;
 	}
+	return count;
 }
+
 int fse_removeFeatures(MD5 *md5s, size_t md5Count) {
 	if (nullptr == md5s) {
 		return 0;
@@ -181,36 +155,52 @@ int fse_removeFeatures(MD5 *md5s, size_t md5Count) {
 	if (!valid_instance()) {
 		return -1;
 	}
-	int count = 0,result;
-	for (auto i = md5Count - 1; i >= 0; --i, count += result) {
-		result = fse_removeFeature(md5s + i);
-		if (result < 0) {
-			return -1;
+	int count = 0;
+	bool result;
+	try {
+		for (decltype(md5Count) i = 0; i < md5Count; ++i) {
+			if (!md5s[i].is_null()) {
+				result = instance->removeBean(md5s[i]);
+				count += result ? 1 : 0;
+			}
 		}
+	}
+	catch (exception &e) {
+		fill_error_msg(e.what());
+		return -1;
 	}
 	return count;
 }
-int fse_removeFeaturesByImgMD5(MD5 *imgMD5) {
-	if (is_null_MD5(imgMD5)) {
+
+int fse_removeFeaturesByImgMD5(MD5 *imgMD5s, size_t md5Count) {
+	if (nullptr == imgMD5s) {
 		return 0;
 	}
 	if (!valid_instance()) {
 		return -1;
 	}
 	try {
-		return instance->removeBeansByImgMD5(*imgMD5);
+		int count = 0;
+		for (decltype(md5Count) i = 0; i < md5Count; ++i) {
+			if(!imgMD5s[i].is_null()){
+				count += instance->removeBeansByImgMD5(imgMD5s[i]);
+			}
+		}
+		return count;
 	}
 	catch (exception &e) {
 		fill_error_msg(e.what());
 		return -1;
 	}
 }
+
 int fse_size() {
 	if (!valid_instance()) {
 		return -1;
 	}
 	return instance->size();
 }
+
 static thread_local std::string tls_str_statinfo;
 const char* fse_statInfo() {
 	if (!valid_instance()) {
--- feature_se/feature_se.h
+++ feature_se/feature_se.h
@@ -37,33 +37,25 @@ FSE_API int fse_release();
  */
 FSE_API int fse_searchCode(const face_code *code, double threshold, size_t rows, code_bean*out, const MD5 *md5Arrays, size_t md5Count);
 /* 根据特征码ID在表中查找指定的记录
- * md5 特征码ID
- * out 结果输出
+ * md5 特征码ID,为null时返回0
+ * out 结果输出,为null时出错
  * 返回 0没有找到,1找到,<0 出错
  */
 FSE_API int fse_getFeature(MD5 *md5, code_bean* out);
-/* 添加一条特征码到内存表
- * 正常返回0,< 0 出错
- */
-FSE_API int fse_addFeature(code_bean* bean);
 /* 添加一组特征码到内存表
-* 正常返回添加的特征记录数,< 0 出错
+* 正常返回添加的特征记录数,beans为null时返回0,< 0 出错
 */
-FSE_API int fse_addFeatures(code_bean *beans, size_t beanCount);
+FSE_API int fse_addFeatures(code_bean *beans, size_t beanCount = 1);
 
-/* 删除md5指定的特征
- * 删除成功返回1,失败返回0,< 0出错
- */
-FSE_API int fse_removeFeature(MD5 *md5);
 /* 删除md5s指定的一组特征
-* 删除成功返回删除的记录数,失败返回0,< 0出错
+* 删除成功返回删除的记录数,< 0出错
 */
-FSE_API int fse_removeFeatures( MD5 *md5s, size_t md5Count);
-/* 根据图像imgMD5删除所有指定的记录
- * imgMD5 特征码所在图像的MD5校验码
+FSE_API int fse_removeFeatures( MD5 *md5s, size_t md5Count = 1);
+/* 根据图像imgMD5数组删除所有指定的记录
+ * imgMD5s 特征码所在图像的MD5校验码数组
  * 返回返回删除的记录数,< 0 出错
  */
-FSE_API int fse_removeFeaturesByImgMD5(MD5 *imgMD5);
+FSE_API int fse_removeFeaturesByImgMD5(MD5 *imgMD5s, size_t md5Count = 1);
 /* 返回内存表中元素个数 */
 FSE_API int fse_size();
 /* 返回哈希表统计信息字符串指针(当前线程有效,用于debug测试),出错返回nullptr */
