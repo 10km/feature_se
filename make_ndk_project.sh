8af16ab89957afb2974a9afa333e2840d4965388
10km <10km0811@sohu.com>
2019-02-21 22:30:35 +0800

    COMMIT

--- make_ndk_project.sh
+++ make_ndk_project.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Android NDK 交叉编译脚本,创建Makefile
+# author guyadong
 
-[ -z "$ANDROID_NDK" ] && echo "environment variable ANDROID_NDK not define" && exit -1
+# 检测是否安装NDK,没有安装NDK则报错退出
+[ -z "$ANDROID_NDK" ] && echo "ERROR:environment variable ANDROID_NDK not define" && exit -1
 
 sh_folder=$(dirname $(readlink -f $0))
 folder_name=$(basename $sh_folder) 
 # 定义编译的版本类型(DEBUG|RELEASE)
-build_type=DEBUG
+build_type=RELEASE
 typeset -u arg1=$1
 [ "$arg1" = "DEBUG" ] && build_type=$arg1
 echo build_type=$build_type
@@ -22,11 +25,7 @@ cmake "$sh_folder" -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=$build_
 	-DCMAKE_SYSTEM_NAME=Android \
 	-DCMAKE_SYSTEM_VERSION=17 \
 	-DANDROID_ARM_NEON=ON \
-	-DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 \
 	-DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
-
-# -DCMAKE_ANDROID_STANDALONE_TOOLCHAIN=$ANDROID_NDK
-#	-DCMAKE_ANDROID_NDK=$ANDROID_NDK 
 
 popd
 popd
