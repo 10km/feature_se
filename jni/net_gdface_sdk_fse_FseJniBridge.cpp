0d82edc909eb711adb3f92da240e83601aa000ff
10km <10km0811@sohu.com>
2019-03-05 15:13:22 +0800

    删除无用的代码

--- jni/net_gdface_sdk_fse_FseJniBridge.cpp
+++ jni/net_gdface_sdk_fse_FseJniBridge.cpp
@@ -168,28 +168,3 @@ JNIEXPORT jstring JNICALL Java_net_gdface_sdk_fse_FseJniBridge_statInfo
 	}
 	return jni_utilits::raii_NewStringUTF(info).norelease().get();
 }
-//JNIEXPORT jstring JNICALL Java_net_gdface_sdk_fse_FseJniBridge_md5
-//(JNIEnv *env, jclass, jbyteArray input) {
-//	if (nullptr == input) {
-//		return nullptr;
-//	}
-//	auto byte_ptr = jni_utilits::raii_GetByteArrayElements(input);
-//	auto codeMD5 = md5::digestString(byte_ptr.get(), (unsigned int)env->GetArrayLength(input));
-//	return jni_utilits::raii_NewStringUTF(codeMD5.data()).norelease().get();
-//}
-//#ifdef CUSTOM_FEACOMP
-//#include "custom_feature_compare.h"
-//
-//JNIEXPORT jdouble JNICALL Java_net_gdface_sdk_fse_FseJniBridge_compare
-//(JNIEnv *env, jclass, jbyteArray f1, jbyteArray f2) {
-//	if (nullptr == f1 || nullptr == f1) {
-//		return 0;
-//	}
-//	face_code code1,code2;
-//	auto result = BeanUtilits::jbytearraytoface_code(f1, code1);
-//	result &= BeanUtilits::jbytearraytoface_code(f2, code2);
-//	if(result)
-//		return gdface::feature::compare(code1, code2);
-//	return 0;
-//}
-//#endif
--- jni/net_gdface_sdk_fse_FseJniBridge.h
+++ jni/net_gdface_sdk_fse_FseJniBridge.h
@@ -78,10 +78,6 @@ JNIEXPORT jint JNICALL Java_net_gdface_sdk_fse_FseJniBridge_size
  */
 JNIEXPORT jstring JNICALL Java_net_gdface_sdk_fse_FseJniBridge_statInfo
   (JNIEnv *, jclass);
-//JNIEXPORT jstring JNICALL Java_net_gdface_sdk_fse_FseJniBridge_md5
-//(JNIEnv *env, jclass, jbyteArray input);
-//JNIEXPORT jdouble JNICALL Java_net_gdface_sdk_fse_FseJniBridge_compare
-//(JNIEnv *env, jclass, jbyteArray f1, jbyteArray f2);
 #ifdef __cplusplus
 }
 #endif
